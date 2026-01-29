51c86f50db7933eae076bd0be117de5c353bde2f
Alba Martinez <25005822@saultcollege.ca>
2026-01-29 18:46:48 -0400

    Lecture 4 in class exercise

--- src/main/java/csd214/bookstore/pojos/Book.java
+++ src/main/java/csd214/bookstore/pojos/Book.java
@@ -1,54 +1,42 @@
 package csd214.bookstore.pojos;
 
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Book extends Publication {
     private String author = "";
-    private String isbn = "";
 
     public Book() {
         super();
     }
 
-    public Book(String author, String isbn) {
+    public Book(String author) {
         this.author = author;
-        this.isbn = isbn;
     }
 
-    public Book(String author, String isbn, String title, double price, int copies) {
+    public Book(String author, String title, double price, int copies) {
         super(title, price, copies);
         this.author = author;
-        this.isbn = isbn;
     }
-
     @Override
-    public void initialize() {
-        // 1. Initialize Parent (Title)
-        super.initialize();
+    public void initialize(Scanner input) {
+        // Pass scanner up to parent
+        super.initialize(input);
 
-        // 2. Initialize Self (Author)
         System.out.println("Enter Author:");
-        this.author = getInput("Unknown Author");
+        this.author = getInput(input, "Unknown Author");
 
-        System.out.println("Enter International Standard Book Number:");
-        this.isbn = getInput("Unknown isbn");
-
-        // 3. Initialize Parent (Copies/Price)
-        super.initPriceCopies();
+        // Pass scanner to helper
+        super.initPriceCopies(input);
     }
 
     @Override
-    public void edit() {
-        // 1. Edit Parent fields (Title, Price, Copies)
-        super.edit();
-
-        // 2. Edit Self fields
+    public void edit(Scanner input) {
+        super.edit(input);
         System.out.println("Edit Author [" + this.author + "]:");
-        this.author = getInput(this.author);
-
-        System.out.println("Edit Isbn [" + this.isbn + "]:");
-        this.isbn = getInput(this.isbn);
+        this.author = getInput(input, this.author);
     }
+
 
     @Override
     public void sellItem() {
@@ -59,33 +47,27 @@ public class Book extends Publication {
     public String getAuthor() {
         return author;
     }
-    public String getIsbn() { return isbn; }
 
     public void setAuthor(String author) {
         this.author = author;
     }
-    public void getIsbn(String isbn) {
-        this.isbn = isbn;
+
+    @Override
+    public String toString() {
+        return "Book{author='" + author + "', " + super.toString() + "}";
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
         if (!super.equals(o)) return false;
         Book book = (Book) o;
-        return Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn);
+        return Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), author, isbn);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "author='" + author + '\'' +
-                ", isbn='" + isbn + '\'' +
-                '}';
+        return Objects.hash(super.hashCode(), author);
     }
 }
