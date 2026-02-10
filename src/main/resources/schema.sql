47397b245b8f7d686d3574dcc0eb4a532b30a260
Alba Martinez <25005822@saultcollege.ca>
2026-02-10 13:02:25 -0400

    Lab 3 Progress

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS laptops (
     screenSizeInches DOUBLE,
     warrantyMonths DOUBLE
 );
+
+CREATE TABLE IF NOT EXISTS phones (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    brand VARCHAR(255),
+    supports5G BIT,
+    price DOUBLE,
+    copies INT
+    );
